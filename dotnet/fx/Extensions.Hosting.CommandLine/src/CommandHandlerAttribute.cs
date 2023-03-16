7922bbd8f8bd06623b84f5795b35ff01b46a4e0c
bearz-me <119234633+bearz-me@users.noreply.github.com>
2023-03-16 02:35:25 -0500

    fx: import Extensions.Hosting.CommandLine

---
+++ dotnet/fx/Extensions.Hosting.CommandLine/src/CommandHandlerAttribute.cs
@@ -0,0 +1,15 @@
+using System.CommandLine;
+using System.ComponentModel.Composition;
+
+namespace Bearz.Extensions.Hosting.CommandLine;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+public sealed class CommandHandlerAttribute : Attribute
+{
+    public CommandHandlerAttribute(Type commandHandlerType)
+    {
+        this.CommandHandlerType = commandHandlerType;
+    }
+
+    public Type CommandHandlerType { get; }
+}
